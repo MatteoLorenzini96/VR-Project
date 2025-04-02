ae313b5320f47db059177833f3616048c7c23bbf
Matteo Lorenzini <matteolorenzini96.ml@gmail.com>
2025-04-02 15:19:11 +0200

    added Bomb prefab and scripts for controlling

---
+++ VR Project/Assets/SnowFight/Scripts/Bombs/BombAI.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class BombAI : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float _explosionRadius = 5f;
+    [SerializeField] private float _moveSpeed = 3f;
+
+    [Header("VFX and SFX")]
+    [SerializeField] private string _explosionVFXName = "ExplosionEffect";
+    [SerializeField] private string _explosionSFXName = "ExplosionSound";
+
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        SearchForPlayer();
+    }
+
+    private void SearchForPlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            _playerTransform = player.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Player non trovato! Assicurati che l'oggetto abbia il tag 'Player'.");
+        }
+    }
+
+    private void Update()
+    {
+        if (_playerTransform != null)
+        {
+            MoveTowardsPlayer();
+
+            float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
+
+            if (distanceToPlayer <= _explosionRadius)
+            {
+                Explode();
+            }
+        }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        Vector3 directionToPlayer = (_playerTransform.position - transform.position).normalized;
+        transform.position += directionToPlayer * _moveSpeed * Time.deltaTime;
+    }
+
+    private void Explode()
+    {
+        Debug.Log("Esplosione avvenuta");
+
+        VFXManager.Instance.SpawnEffect(_explosionVFXName, transform.position, Quaternion.identity);
+        AudioManager.Instance.PlaySFX(_explosionSFXName);
+
+        // Aggiungere codice per i danni (se necessario)
+
+        Destroy(gameObject);
+    }
+}
