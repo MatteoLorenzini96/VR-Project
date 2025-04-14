022e099b74393da10d1f79e520dd88b2ea5ec80e
Matteo Lorenzini <matteolorenzini96.ml@gmail.com>
2025-04-14 19:16:55 +0200

    added HP visualization

--- VR Project/Assets/SnowFight/Scripts/Test/HpText.cs
+++ VR Project/Assets/SnowFight/Scripts/Test/HpText.cs
@@ -6,10 +6,44 @@ public class HpText : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _hpText;
     [SerializeField] private HealthManager _healthManager;
     [SerializeField] private GameObject _panel;
+    [SerializeField] private float stillThreshold = 0.001f; // Sensibilità movimento
+    [SerializeField] private float stillTimeRequired = 0.5f; // Tempo necessario da fermo
+
+    private Vector3 _lastPosition;
+    private float _stillTimer = 0f;
+    private bool _isPanelVisible = false;
 
     private void Start()
     {
+        _lastPosition = transform.position;
         UpdateHpText();
+        SetPanelVisible(false);
+    }
+
+    private void Update()
+    {
+        float distanceMoved = Vector3.Distance(transform.position, _lastPosition);
+
+        if (distanceMoved < stillThreshold)
+        {
+            _stillTimer += Time.deltaTime;
+
+            if (_stillTimer >= stillTimeRequired && !_isPanelVisible)
+            {
+                SetPanelVisible(true);
+            }
+        }
+        else
+        {
+            _stillTimer = 0f;
+
+            if (_isPanelVisible)
+            {
+                SetPanelVisible(false);
+            }
+        }
+
+        _lastPosition = transform.position;
     }
 
     public void UpdateHpText()
@@ -20,11 +54,12 @@ public class HpText : MonoBehaviour
         }
     }
 
-    public void TogglePanel()
+    private void SetPanelVisible(bool visible)
     {
         if (_panel != null)
         {
-            _panel.SetActive(!_panel.activeSelf);
+            _panel.SetActive(visible);
+            _isPanelVisible = visible;
         }
     }
 }
